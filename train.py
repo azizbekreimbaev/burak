50fd02c6db875d1ecda8dfad6a38ee3ca408faf3
Azizbek <azizbekreimbaev1003@gmail.com>
2026-05-22 18:52:41 +0900

    feat: TASK L

--- train.py
+++ train.py
@@ -1,3 +1,19 @@
 # TASK L
 # So'zlarni ketma-ketligini buzmasdan har bir so'zni alohida teskarisiga o'girib beradigan function tuzing.
 # Masalan: reverseSentence("we like coding!") return "ew ekil !gnidoc"
+
+arr = []
+
+
+def reverse(str):
+    # print(str.split())
+    for item in str.split():
+        # print(item)
+        print(item[::-1])
+        arr.append(item[::-1])
+        print(arr)
+
+    return " ".join(arr)
+
+
+print(reverse("we like coding!"))
