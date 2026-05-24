69ae29ed2f4e759a2628891541e9dc121d08288f
Azizbek <azizbekreimbaev1003@gmail.com>
2026-05-24 20:44:42 +0900

    feat: created train.ts

--- src/server.ts
+++ src/server.ts
@@ -24,34 +24,3 @@ dotenv.config();
 
 
 
-
-// console.log('====================================');
-// console.log("Executed");
-// console.log('====================================');
-
-// // const person: string = "Kevin";
-// // const age: number = 25;
-
-// import moment from 'moment'
-
-// const time = moment().format("YYYY MM DD")
-// console.log(time)
-// // interface setting type for objects
-
-// interface Person {
-//     name: string,
-//     age: number
-// }
-
-// let person: Person = {
-//     name: "Kevin",
-//     age: 25
-// };
-
-// console.log(person.name)
-
-// // person = {
-// //     name: "Kevin",
-// //     age: 25
-// // }
-
