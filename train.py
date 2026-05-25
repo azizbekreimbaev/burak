3346fd14c1360acd3aa93784122ab643e276d84c
Azizbek <azizbekreimbaev1003@gmail.com>
2026-05-26 00:39:38 +0900

    feat: TASK M

--- train.py
+++ train.py
@@ -1,19 +1,40 @@
-# TASK L
-# So'zlarni ketma-ketligini buzmasdan har bir so'zni alohida teskarisiga o'girib beradigan function tuzing.
-# Masalan: reverseSentence("we like coding!") return "ew ekil !gnidoc"
 
-arr = []
+# // TASK M
 
+# // Array ichidagi har bir raqam uchun raqamning o'zi va uning kvadratidan tashkil topgan object hosil qilib qaytarsin.
 
-def reverse(str):
-    # print(str.split())
-    for item in str.split():
-        # print(item)
-        print(item[::-1])
-        arr.append(item[::-1])
-        print(arr)
-
-    return " ".join(arr)
+# // Masalan: getSquareNumbers([1, 2, 3]) return [{ number: 1, square: 1 }, ...]
 
 
-print(reverse("we like coding!"))
+def name(arr):
+    result = []
+    for item in arr:
+        a = f"number:{item}, square: {item*item}"
+        print(a)
+        result.append({a})
+
+    return result
+
+
+print(name([1, 2]))
+
+
+# # TASK L
+# # So'zlarni ketma-ketligini buzmasdan har bir so'zni alohida teskarisiga o'girib beradigan function tuzing.
+# # Masalan: reverseSentence("we like coding!") return "ew ekil !gnidoc"
+
+# arr = []
+
+
+# def reverse(str):
+#     # print(str.split())
+#     for item in str.split():
+#         # print(item)
+#         print(item[::-1])
+#         arr.append(item[::-1])
+#         print(arr)
+
+#     return " ".join(arr)
+
+
+# print(reverse("we like coding!"))
