3dc1b6cce26419d4bea43a0e159154241bebc0f0
Azizbek <azizbekreimbaev1003@gmail.com>
2026-05-31 22:28:05 +0900

    feat: created get and post with POSTMAN

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -3,6 +3,12 @@ import { Request, Response } from 'express'
 const restaurantController: T = {};
 import MemberService from '../models/Member.service'
 
+
+
+
+/** Restaurant */
+
+// send | json | redirect | end | render
 restaurantController.goHome = (req: Request, res: Response) => {
     try {
         console.log("goHome")
@@ -32,5 +38,30 @@ restaurantController.getSignup = (req: Request, res: Response) => {
     }
 
 };
+
+
+restaurantController.processLogin = (req: Request, res: Response) => {
+    try {
+        console.log("processLogin")
+        res.send("POSTMAN DONE")
+    } catch (err) {
+        console.log("ERROR, getsignUp:", err)
+    }
+
+};
+
+restaurantController.processSignup = (req: Request, res: Response) => {
+    try {
+        console.log("processSignup")
+        res.send("POSTMAN SIGNUP DONE")
+    } catch (err) {
+        console.log("ERROR, getsignUp:", err)
+    }
+
+};
+
+/** Product */
+/** User */
+
 
 export default restaurantController;
