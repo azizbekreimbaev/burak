dc50bc5e510a4c12a54f2b8da54023d30157325e
Azizbek <azizbekreimbaev1003@gmail.com>
2026-06-10 19:57:34 +0900

    feat: develop upload product files to server by multer

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -22,7 +22,8 @@ productController.getAllProducts = async (req: Request, res: Response) => {
 
 productController.createNewProduct = async (req: Request, res: Response) => {
     try {
-        console.log("createNewProduct")
+        console.log("createNewProduct");
+        res.send("Done")
     } catch (err) {
         console.log("ERROR, createNewProduct:", err)
         if (err instanceof Errors) res.status(err.code).json(err);
--- src/routera-admin.ts
+++ src/routera-admin.ts
@@ -2,6 +2,7 @@ import express from 'express';
 const routerAdmin = express.Router();
 import restaurantController from './controllers/restaurant.controller'
 import productController from './controllers/product.controller';
+import makeUploader from './libs/utils/uploader';
 
 routerAdmin.get("/", restaurantController.goHome);
 
@@ -11,16 +12,25 @@ routerAdmin
 
 routerAdmin
     .get("/signup", restaurantController.getSignup)
-    .post("/signup", restaurantController.processSignup);
+    .post(
+        "/signup",
+        makeUploader("members").single("memberImage"),
+        restaurantController.processSignup);
 
 routerAdmin.get("/logout", restaurantController.logout);
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 
 /* Product*/
-routerAdmin.get(
-    "/product/all",
+routerAdmin.get("/product/all", restaurantController.verifyRestaurant, productController.getAllProducts);
+
+routerAdmin.post("/product/create",
     restaurantController.verifyRestaurant,
-    productController.getAllProducts);
-routerAdmin.post("/product/create", productController.createNewProduct);
-routerAdmin.post("/product/:id", productController.updateChosenProduct);
+    makeUploader("products").array("productImages", 5),
+    productController.createNewProduct);
+
+routerAdmin.post("/product/:id",
+    restaurantController.verifyRestaurant,
+    productController.updateChosenProduct);
+
+
 export default routerAdmin;
