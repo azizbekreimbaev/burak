d5612829c0c75cec174922b17efff40b8eb5406a
Azizbek <azizbekreimbaev1003@gmail.com>
2026-06-11 04:09:52 +0900

    feat: developed product schema model

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,6 +1,14 @@
+import ProductModel from "../schema/Product.model";
 
 
-class ProductService { }
+class ProductService {
+    private readonly productModel;
+
+    constructor() {
+        this.productModel = ProductModel;
+    }
+}
+
 
 
 export default ProductService;
