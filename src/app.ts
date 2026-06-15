33ea93765dcaabef9b5f89fa34d5be454db97b15
Azizbek <azizbekreimbaev1003@gmail.com>
2026-06-15 21:42:23 +0900

    feat: fix file name

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import path from 'path'
 import router from './router'
-import routerAdmin from './routera-admin'
+import routerAdmin from './router-admin'
 import morgan from 'morgan';
 import { MORGAN_FORMAT } from './libs/config';
 
