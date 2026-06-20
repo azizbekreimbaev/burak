5e584f126673944a6f1c392fe2fb887e541ba0ad
Azizbek <azizbekreimbaev1003@gmail.com>
2026-06-20 18:30:10 +0900

    feat: developed preloading of image

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -7,6 +7,33 @@ console.log("Signup frontend javascript file");
 // })
 
 
+$(function () {
+    fileTarget = $(".file-box .upload-hidden");
+
+    let filename;
+
+    fileTarget.on("change", function () {
+        if (window.FileReader) {
+            const uplaodFile = $(this)[0].files[0];
+            const fileType = uplaodFile["type"];
+            const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+            if (!validImageType.includes(fileType)) {
+                alert("please insert only jpeg, jpg, png");
+            } else {
+                if (uplaodFile) {
+                    console.log(URL.createObjectURL(uplaodFile));
+                    $(".upload-img-frame").attr("src", URL.createObjectURL(uplaodFile)).addClass("success");
+                }
+                filename = $(this)[0].files[0].name;
+            }
+
+            $(this).siblings(".upload-name").val(filename);
+        }
+    })
+})
+
+
+
 function validateSignupForm() {
     const memberNick = $(".member-nick").val();
     const memberPhone = $(".member-phone").val();
@@ -21,5 +48,12 @@ function validateSignupForm() {
         alert("Passwords differs, please chech again");
         return false
     }
+
+    const memberImage = $(".member-image").get(0).files[0] ? $(".member-image").get(0).files[0].name : null;
+    if (!memberImage) {
+        alert("Please insert image");
+        return false;
+    }
+
 
 }
