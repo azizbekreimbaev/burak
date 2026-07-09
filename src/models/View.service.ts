1ac7b3d2f0c93b1d77b6a9a3ba0fcded758dc096
Azizbek <azizbekreimbaev1003@gmail.com>
2026-07-09 12:56:17 +0900

    feat: created viewSchema/Service models

---
+++ src/models/View.service.ts
@@ -0,0 +1,15 @@
+import ViewModel from "../schema/View.model";
+
+class ViewService {
+    private readonly viewModel;
+
+    constructor() {
+        this.viewModel = ViewModel;
+    }
+
+
+
+
+}
+
+export default ViewService;
